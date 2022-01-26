e9e640c6a952cd07050fe7715b05509e80748441
Armin Jalali <arminjalali747@gmail.com>
2022-01-26 15:51:46 +0330

    change minor things

--- Reddit/Backend/storage/framework/views/9a91c48e56876cb2c2a74baccb667c7c9994a8bf.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH /Users/amir/PhpstormProjects/note/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
--- Reddit/Backend/storage/framework/views/aee469be07af891278995687d8d4e685c4ac9de6.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH /Users/amir/PhpstormProjects/note/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
